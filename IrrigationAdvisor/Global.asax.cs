2c8f2e2bd224d42c3eff2ee22f1801d2b95ad837
Cristian Gonzalez <cristiangonzalezalvarez@gmail.com>
2016-07-06 20:03:19 -0300

    Comment recreation of the database.

--- IrrigationAdvisor/Global.asax.cs
+++ IrrigationAdvisor/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using IrrigationAdvisor.DBContext;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +14,11 @@ namespace IrrigationAdvisor
     {
         protected void Application_Start()
         {
+
+            //Database.SetInitializer<IrrigationAdvisorContext>
+            //        (new DropCreateDatabaseIfModelChanges<IrrigationAdvisorContext>());
+
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
