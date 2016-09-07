464bd7b47e5e50baa72a53ea5b830cc40e193c42
Cristian Gonzalez <cristiangonzalezalvarez@gmail.com>
2016-09-06 22:52:34 -0300

    Load combo of farms. Implement multi farms.

---
+++ IrrigationAdvisor/Controllers/Helpers/FarmShortView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IrrigationAdvisor.Controllers.Helpers
+{
+    public class FarmShortView
+    {
+        public string ErrorMessage { get; set; }
+        public long FarmId { get; set; }
+        public string FarmDescription { get; set; }
+    }
+}
