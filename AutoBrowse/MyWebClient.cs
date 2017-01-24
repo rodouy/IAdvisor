e5249d952dcff210b3366530ffe3fb97a68e0011
Cristian Gonzalez <cristiangonzalezalvarez@gmail.com>
2017-01-23 21:47:10 -0300

    Update timeout for autobrowse.

--- AutoBrowse/MyWebClient.cs
+++ AutoBrowse/MyWebClient.cs
@@ -12,7 +12,7 @@ namespace AutoBrowse
         protected override WebRequest GetWebRequest(Uri uri)
         {
             WebRequest w = base.GetWebRequest(uri);
-            w.Timeout = 20 * 60 * 1000;
+            w.Timeout = 30 * 60 * 1000;
             return w;
         }
     }
