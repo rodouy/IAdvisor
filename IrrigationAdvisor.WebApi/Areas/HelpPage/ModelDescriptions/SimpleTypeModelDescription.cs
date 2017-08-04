454a9e1cc24c6d19eeba0f37ccf9fa783334f7f8
Cristian Gonzalez <cristiangonzalezalvarez@gmail.com>
2017-08-03 22:07:30 -0300

    Create web api project.

---
+++ IrrigationAdvisor.WebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace IrrigationAdvisor.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ IrrigationAdvisor.WebApi/Models/AddIrrigationOrRainValues.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IrrigationAdvisor.WebApi.Models
+{
+    public class AddIrrigationOrRainValues
+    {
+        public string Token { get; set; }
+        public int IrrigationUnitId { get; set; }
+        public decimal Milimeters { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
