e92140dd64a6dc2db21436458cbed3048284ae02
BHU\cpalo <cgpalo@gmail.com>
2017-09-02 11:34:50 -0300

    Reports PiovotState

--- IrrigationAdvisor/DBContext/Management/DailyRecordConfiguration.cs
+++ IrrigationAdvisor/DBContext/Management/DailyRecordConfiguration.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Data.Entity.ModelConfiguration;
 using IrrigationAdvisor.Models.Management;
+using NLog;
+using System.Data.Entity;
 
 namespace IrrigationAdvisor.DBContext.Management
 {
     public class DailyRecordConfiguration:
         EntityTypeConfiguration<DailyRecord>
     {
+
+        private IrrigationAdvisorContext db = IrrigationAdvisorContext.Instance();
+
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         public DailyRecordConfiguration()
         {
             ToTable("DailyRecord");
@@ -20,7 +27,90 @@ namespace IrrigationAdvisor.DBContext.Management
                 .IsRequired();
             Property(c => c.CropIrrigationWeatherId)
                 .IsRequired();
-            
+
+            Property(c => c.RainId)
+                .IsRequired();
+
         }
+
+        /// <summary>
+        /// Get  DailyRecords List with Rains and Irrigation
+        /// </summary>
+        /// <param name="cropIrrigationWeatherId"></param>
+        /// <returns></returns>
+        public List<DailyRecord> GetDailyRecordsListDataBy(long cropIrrigationWeatherId)
+        {
+            List<DailyRecord> lReturn = null;
+           // List<DailyRecord> lDailyRecordList = new List<DailyRecord>();
+            List<DailyRecord> lDailyRecordList = null;
+            try
+            {
+                if (cropIrrigationWeatherId > 0)
+                {
+                    //lDailyRecordList = (from dr in db.DailyRecords
+                    //                    join wiR in db.WaterInput on dr.RainId equals wiR.WaterInputId into gj
+                    //                    from sub in gj.DefaultIfEmpty()
+                    //                    join wiI in db.WaterInput on dr.IrrigationId equals wiI.WaterInputId into gj1
+                    //                    from sub1 in gj1.DefaultIfEmpty()
+                    //                    where dr.CropIrrigationWeatherId == cropIrrigationWeatherId
+                    //                    select dr).ToList();
+
+
+                    lDailyRecordList = db.DailyRecords
+                                    .Include(d => d.Rain)
+                                    .Include(d => d.Irrigation)
+                                    .Where (d => d.CropIrrigationWeatherId == cropIrrigationWeatherId).ToList();
+
+
+                    //lDailyRecordList = (from dr in db.DailyRecords                                       
+                    //                    where dr.CropIrrigationWeatherId == cropIrrigationWeatherId
+                    //                    select dr).ToList();
+
+
+                    lReturn = lDailyRecordList;
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Exception in DailyRecord.GetDailyRecordsListDataBy " + "\n" + ex.Message + "\n" + ex.StackTrace);
+            }
+
+            return lReturn;
+        }
+
+        ///// <summary>
+        ///// Get a DailyRecord in CropIrrigationWeather list 
+        ///// Where CropIrrigationWeather is the IrrigationUnit instance
+        /////     And DailyRecord Date equals Date of Reference
+        ///// </summary>
+        ///// <param name="pIrrigationUnit"></param>
+        ///// <param name="pDateOfReference"></param>
+        ///// <returns></returns>
+        //public DailyRecord GetDailyRecordBy(long cropIrrigationWeatherId)
+        //{
+        //    DailyRecord lReturn = null;
+        //    DailyRecord lCropIrrigationWeather = null;
+        //    List<DailyRecord> lCropIrrigationWeaterList = new List<DailyRecord>();
+
+           
+        //        lCropIrrigationWeather = db.DailyRecords
+        //            .Include(ciw => ciw.IrrigationList)
+        //            .Include(ciw => ciw.MainWeatherStation)
+        //            .Include(ciw => ciw.AlternativeWeatherStation)
+        //            .Include(ciw => ciw.PhenologicalStageAdjustmentList)
+        //            .Include(ciw => ciw.Soil)
+        //            .Include(ciw => ciw.DailyRecordList)
+        //            .Where(ciw => ciw.IrrigationUnitId == pIrrigationUnit.IrrigationUnitId
+        //                    && ciw.CropId == pCrop.CropId
+        //                    && ciw.SowingDate <= pDateOfReference
+        //                    && ciw.HarvestDate >= pDateOfReference).FirstOrDefault();
+
+        //        lReturn = lCropIrrigationWeather.DailyRecordList
+        //            .Where(ciw => ciw.DailyRecordDateTime.Date == pDateOfReference.Date).FirstOrDefault();
+        //    }
+
+        //    return lReturn;
+        //}
+
     }
 }
---
+++ IrrigationAdvisor/ViewModels/Reports/ReportPivotStateViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Helpers;
+
+namespace IrrigationAdvisor.ViewModels.Reports
+{
+    public class ReportPivotStateViewModel
+    {
+        [Required]
+        public Chart Chart { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N0}", ApplyFormatInEditMode = true)]
+        [Required]
+        public double TotalEffectiveInputWater { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:N0}", ApplyFormatInEditMode = true)]
+        public double TotalEffectiveRain { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N0}", ApplyFormatInEditMode = true)]
+        [Required]
+        public double TotalEvapotranspirationCrop { get; set; }
+
+        public bool IsUserAdministrator { get; set; }
+
+    }
+
+}
