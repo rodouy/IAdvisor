058fd3ee6c3d05e00166b0a11e1fb224ddf5728e
Cristian Palo <cgpalo@gmail.com>
2018-08-16 16:04:19 -0300

    Fix visual details

--- IrrigationAdvisor/Controllers/Localization/FarmsController.cs
+++ IrrigationAdvisor/Controllers/Localization/FarmsController.cs
@@ -16,6 +16,9 @@ using IrrigationAdvisor.Models.Security;
 using IrrigationAdvisor.DBContext.Weather;
 using IrrigationAdvisor.DBContext.Localization;
 using IrrigationAdvisor.ViewModels.Localization;
+using Newtonsoft.Json;
+using IrrigationAdvisor.Models.Irrigation;
+using IrrigationAdvisor.Models.Utilities;
 
 namespace IrrigationAdvisor.Controllers.Localization
 {
@@ -92,24 +95,90 @@ namespace IrrigationAdvisor.Controllers.Localization
             return View();
         }
 
-        // POST: Farms/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // POST: Users/Create;
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "FarmId,Name,Address,Phone,Has")] Farm farm)
+        public ActionResult Create([Bind(Include = "Name, Company, Phone, Address, Latitude, Longitude, Has, WeatherStationId, CityId, BombsHidden")] WizardFarmViewModel vm)
         {
+
             if (ModelState.IsValid)
             {
-                db.Farms.Add(farm);
+                Farm farmMapped = new Farm();
+                long lFarmPositionId = GetPositionId(vm.Latitude, vm.Longitude);
+                long lBombPositionId;
+
+                Position positionFarm = new Position();
+                //Not exist position to farm
+                if (lFarmPositionId == 0)
+                {
+                    positionFarm.Latitude = vm.Latitude;
+                    positionFarm.Longitude = vm.Longitude;
+                    positionFarm.Name = vm.Name + " - Establecimiento";
+                    farmMapped.Position = positionFarm;
+                }
+                else
+                {
+                    farmMapped.PositionId = lFarmPositionId;
+                }
+
+                // working whit bombs
+                dynamic items = JsonConvert.DeserializeObject(vm.BombsHidden);
+                foreach (var item in items.items)
+                {
+                    double bombLatitud = Convert.ToDouble((String)(item.bombLatitude));
+                    double bombLongitude = Convert.ToDouble((String)item.bombLongitude);
+
+                    Bomb newbomb = new Bomb();
+                    newbomb.Name = vm.Name + " - " + item.bombShortName;
+                    newbomb.ShortName = item.bombShortName;
+                    newbomb.SerialNumber = item.serialNumber;
+
+                    newbomb.ServiceDate = ((String)item.serviceDate == "") ? Utils.MIN_DATETIME : item.serviceDate;
+                    newbomb.PurchaseDate = ((String)item.purchaseDate == "") ? Utils.MIN_DATETIME : item.purchaseDate;
+
+                    //position bomb is diferent to position farm, find position to bomb
+                    if (vm.Latitude != bombLatitud || vm.Longitude != bombLongitude)
+                    {
+                        lBombPositionId = GetPositionId((double)item.bombLatitude, (double)item.bombLongitude);
+
+                        //Not exist position to bomb
+                        if (lBombPositionId == 0)
+                        {
+                            Position positionBomb = new Position();
+                            positionBomb.Latitude = item.bombLatitude;
+                            positionBomb.Longitude = item.bombLongitude;
+                            positionBomb.Name = Convert.ToString(item.bombLatitude) + " - " + Convert.ToString(item.bombLatitude);
+                            newbomb.Position = positionBomb;
+                        }
+                        else
+                        {
+                            newbomb.PositionId = lBombPositionId;
+                        }
+                    }
+                    else
+                    {
+                        newbomb.PositionId = lFarmPositionId;
+                    }
+
+                    farmMapped.AddBomb(newbomb);
+                }
+
+                farmMapped.Name = vm.Name;
+                farmMapped.Company = vm.Company;
+                farmMapped.Address = vm.Address;
+                farmMapped.Phone = vm.Phone;
+                farmMapped.Has = vm.Has;
+                farmMapped.WeatherStationId = vm.WeatherStationId;
+                farmMapped.CityId = vm.CityId;
+
+                db.Farms.Add(farmMapped);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+
+
             }
+            return View("~/Views/Localization/Farms/Index.cshtml", db.Farms.ToList());
 
-            return View(farm);
         }
-
-
 
         // POST: Farms/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
@@ -292,6 +361,74 @@ namespace IrrigationAdvisor.Controllers.Localization
             return Json(new { success = true, responseText = lCity.CityId }, JsonRequestBehavior.AllowGet);
 
         }
-    }
+   
+            #region private methondaux
 
+        private long GetPositionId(double pLatitude, double pLongitude)
+        {
+            PositionConfiguration pc;
+            pc = new PositionConfiguration();
+            return pc.GetPositionIdByLatitudLongitud(pLatitude, pLongitude);
+        }
+
+        private List<System.Web.Mvc.SelectListItem> LoadCities(long? cityId = null, City city = null)
+        {
+
+            CityConfiguration cc = new CityConfiguration();
+
+            List<City> cities = cc.GetAllCities();
+            List<System.Web.Mvc.SelectListItem> result = new List<SelectListItem>();
+
+            foreach (var item in cities)
+            {
+
+                bool isSelected = false;
+                if (city != null && cityId.HasValue)
+                {
+                    isSelected = (city.CityId == cityId);
+                }
+
+                SelectListItem sl = new SelectListItem()
+                {
+                    Value = item.CityId.ToString(),
+                    Text = item.Name,
+                    Selected = isSelected
+                };
+
+                result.Add(sl);
+            }
+
+            return result;
+        }
+
+        private List<System.Web.Mvc.SelectListItem> LoadWeatherStations(long? weatherStationId = null, WeatherStation weatherStation = null)
+        {
+            WeatherStationConfiguration wsc = new WeatherStationConfiguration();
+
+            List<WeatherStation> weatherStations = wsc.GetAllWeatherStations();
+            List<System.Web.Mvc.SelectListItem> result = new List<SelectListItem>();
+
+            foreach (var item in weatherStations)
+            {
+
+                bool isSelected = false;
+                if (weatherStation != null && weatherStationId.HasValue)
+                {
+                    isSelected = (weatherStation.WeatherStationId == weatherStationId);
+                }
+
+                SelectListItem sl = new SelectListItem()
+                {
+                    Value = item.WeatherStationId.ToString(),
+                    Text = item.Name,
+                    Selected = isSelected
+                };
+
+                result.Add(sl);
+            }
+
+            return result;
+        }
+        #endregion
+    }
 }
