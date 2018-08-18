77ff7f7bc1508d2122264647b35ff7d11057735d
Cristian Gonzalez <cristiangonzalezalvarez@gmail.com>
2018-08-18 11:47:37 -0300

    Meteoblue service.

---
+++ MeteoblueWeatherService/Model/AgroDataUnits.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeteoblueWeatherService.Model
+{
+    public class AgroDataUnits
+    {
+        public string time { get; set; }
+        public string leafwetness { get; set; }
+        public string soilmoisture { get; set; }
+        public string sensibleheatflux { get; set; }
+        public string temperature { get; set; }
+        public string transpiration { get; set; }
+    }
+}
